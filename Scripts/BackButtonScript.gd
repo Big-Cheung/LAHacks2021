a8aa2a1c860dfcd662f0c4f34cefc31a5072856c
Evan Cheung <evan.d.cheung@gmail.com>
2021-03-22 17:32:34 -0700

    Added back button

--- Scripts/BackButtonScript.gd
+++ Scripts/BackButtonScript.gd
@@ -6,11 +6,16 @@ extends Button
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _init():
+	self.connect("pressed", self, "_button_pressed")
 
-func _input(event):
-	pass
+func _button_pressed():
+	self.get_parent().visible = false
+	get_node("../MainPage").visible = true
+	get_node("../MainPage").get_tree().paused = false
+	get_tree().paused = true
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
