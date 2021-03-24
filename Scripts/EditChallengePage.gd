f68e5ebf4f50f060a23b475dfef1b990363b01b9
Evan Cheung <evan.d.cheung@gmail.com>
2021-03-23 19:33:06 -0700

    Edit Challenge page almost done

--- Scripts/EditChallengePage.gd
+++ Scripts/EditChallengePage.gd
@@ -1,22 +1,32 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
-func _init():
-	$Container/NewEvent.connect("pressed", self, "createEvent")
+func _ready():
+	$"Container/NewEvent".connect("pressed", self, "createEvent")
+	$"Container/EventMenu/DeleteEvent".connect("pressed", self, "deleteEvent")
+	$"Container/EventList".connect("item_selected", self, "showMenu")
+	$"Container/EventList".connect("nothing_selected", self, "hideMenu")
 
-func  createEvent():
-	$Container/EventList.add_item("Blank Event")
-	$Container/EventList.select($"../EventList".get_item_count() - 1)
+func createEvent():
+	$"Container/EventList".add_item("Blank Event")
+	$"Container/EventList".select($"Container/EventList".get_item_count() - 1)
+	$"Container/EventList".emit_signal("item_selected", $"Container/EventList".get_item_count() - 1)
 	
 func deleteEvent():
+	$"Container/EventList".remove_item($"Container/EventList".get_selected_items()[0])
+	$"Container/EventList".unselect_all()
+	$"Container/EventList".emit_signal("nothing_selected")
+
+func showMenu(index):
+	$"Container/EventMenu".visible = true
+
+func hideMenu():
+	$"Container/EventMenu".visible = false
+
+
 	
-	$Container/EventList.emit_signal("nothing_selected")
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
