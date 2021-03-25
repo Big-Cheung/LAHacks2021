c9c351bca4819f14880bbedc3e0902194dfabdb5
Evan Cheung <evan.d.cheung@gmail.com>
2021-03-25 14:27:32 -0700

    pushing temp updates

--- GauntletPage.gd
+++ GauntletPage.gd
@@ -5,13 +5,9 @@ extends Control
 # var a = 2
 # var b = "text"
 
-var user = 10392
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#$"Panel/Container/Leaderboard/ItemList".add_item("Evan Cheung",load("res://Icons/Trophy.png"),false)
-	var data = JSON.parse(JsonSTRING)
-	$"Panel/Container/BackButton".text = data.$user.name
-	
 	pass
 
 
